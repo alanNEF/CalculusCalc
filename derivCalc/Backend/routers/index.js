586974c20b4b13bdea66406fc4a890a3c717923e
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-03-31 19:57:21 -0400

    Fixed body undefined error

--- derivCalc/Backend/routers/index.js
+++ derivCalc/Backend/routers/index.js
@@ -8,7 +8,7 @@ require('nerdamer/Calculus');
 
 router.post('/create', uploads.none(), (req, res) => {
   let func = nerdamer.diff(req.body.function);
-  console.log('body', req.body);
+  console.log('body', req.body.function);
   console.log(`${req.body.function}`);
   res.json({success: true, ans: `${func.toString()}`});
 });
