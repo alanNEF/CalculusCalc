cf53948b75330cb530e2e94528985df03b684cf8
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-03-31 18:39:38 -0400

    Added router logic

--- derivCalc/Backend/routers/index.js
+++ derivCalc/Backend/routers/index.js
@@ -5,3 +5,12 @@ const storage = multer.memoryStorage();
 const uploads = multer();
 const nerdamer = require('nerdamer');
 require('nerdamer/Calculus');
+
+router.post('/create', uploads.none(), (req, res) => {
+  let func = nerdamer.diff(req.body.function);
+  console.log('body', req.body);
+  console.log(`${req.body.function}`);
+  res.json({success: true, ans: `${func.toString()}`});
+});
+
+module.exports = router;
