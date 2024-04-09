f6ed0e4c7089ff994292b9cbeb2a2c9e9414f81c
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-04-08 22:54:55 -0400

    updated div router with new path

--- derivCalc/Backend/routers/index.js
+++ derivCalc/Backend/routers/index.js
@@ -6,10 +6,11 @@ const uploads = multer();
 const nerdamer = require('nerdamer');
 require('nerdamer/Calculus');
 
-router.post('/create', uploads.none(), (req, res) => {
+router.post('/createdev', uploads.none(), (req, res) => {
   let func = nerdamer.diff(req.body.function);
   console.log('body', req.body.function);
   console.log(`${req.body.function}`);
+  console.log(`result${func.toString()}`);
   res.json({success: true, ans: `${func.toString()}`});
 });
 
